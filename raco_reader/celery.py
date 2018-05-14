fb777a8ed57e275ad2915ceed84430ee8e42ffa8
annaturupi <annaturupi@gmail.com>
2018-05-14 16:45:51 +0200

    Store all notifications

--- raco_reader/celery.py
+++ raco_reader/celery.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from celery import Celery
 from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.local')
 
 app = Celery('raco_reader')
 app.config_from_object('raco_reader.settings')
@@ -13,8 +13,8 @@ app.config_from_object('raco_reader.settings')
 app.autodiscover_tasks(lambda: ['raco_reader'])
 
 app.conf.beat_schedule = {
-    'print_stuff_crontab': {
-        'task': 'raco_reader.tasks.print_stuff',
-        'schedule': timedelta(seconds=5),  # change to `crontab(minute=0, hour=0)` if you want it to run daily at midnight
-    },
+    'Print notification titles': {
+        'task': 'raco_reader.tasks.store_notifications',
+        'schedule': timedelta(seconds=30),  # change to `crontab(minute=0, hour=0)` if you want it to run daily at midnight
+    }
 }
--- raco_reader/tasks.py
+++ raco_reader/tasks.py
@@ -1,5 +1,30 @@
+from django.contrib.auth.models import User
+
+from main.models import Post
 from raco_reader.celery import app
+from main.oauth import oauth
 
 @app.task
 def print_stuff():
     print("background task")
+
+@app.task
+def store_notifications():
+    #for each user in the model
+    all_users = User.objects.all()
+    for user in all_users:
+        try:
+            token = user.oauth2token
+            resp = oauth.raco.get('/v2/jo/avisos/?format=json', token=token.to_token())
+            profile = resp.json()
+            for note in profile['results']:
+                model = Post(title=note['titol'],
+                             subject=note['codi_assig'],
+                             modification_date=note['data_modificacio'],
+                             text=note['text'],
+                             user = user)
+                print(model.title)
+                model.save()
+        except Exception as inst:
+            print(type(inst))
+
